a6cbed8cb79289900f7094de8c26591ca6de6a01
ilhanneziric <ilhan.neziric@edu.fit.ba>
2021-10-07 00:19:12 +0200

    first commit

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import './HomeScreen.css';
+import Product from '../components/Product.js';
+
+const HomeScreen = () => {
+    return <div className="homescreen">
+        <h2 className="homescreen__title">Latest Products</h2>
+        <div className="homescreen__products">
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+        </div>
+    </div>
+}
+
+export default HomeScreen;
